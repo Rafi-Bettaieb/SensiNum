9fa9ba350c37673d29a7fb417286ea2057ab7aa3
IliasseB <iliasse.bellouch@edu.univ-paris13.fr>
2025-12-05 06:12:27 +0100

    Résolution du problème de nav barre et chagement du style du chatbox

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 // Import du conteneur principal du chatbot
 import ChatbotIndex from './components/chatbot/ChatbotIndex.vue';
+import Header from './components/layout/Header.vue' 
+import Footer from './components/layout/Footer.vue'
 </script>
 
 <template>
   <div class="app-container">
+        <Header />
+
     <router-view />
     <ChatbotIndex />
+        <Footer />
   </div>
 </template>
 
