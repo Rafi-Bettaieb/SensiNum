0aa71e5ff1beed40ac614fcaed9c601526555488
Rafi Bettaieb <rafii.bettaieb004@gmail.com>
2025-12-05 19:33:03 +0100

    ajout de la variable app

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const app = express();
+
 // Middlewares globaux
 app.use(cors());
 app.use(express.json());
